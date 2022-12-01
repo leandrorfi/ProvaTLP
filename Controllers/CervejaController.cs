17d2b8b92fa31918344ad76181dd331244db3872
Arthuthu <arthurgeromello@hotmail.com>
2022-11-30 21:54:59 -0300

    Index e Details da controller cerveja adicionados

--- Controllers/CervejaController.cs
+++ Controllers/CervejaController.cs
@@ -1,4 +1,4 @@
-﻿using ProvaTLP.Models;
+﻿using ProvaLibrary.Repositorios;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,78 +10,22 @@ namespace ProvaTLP.Controllers
     public class CervejaController : Controller
     {
         // GET: Cerveja
-        public ActionResult Adicionar()
+        public ActionResult Index()
         {
+            var cervejas = new CervejasRepositorio();
 
-            ViewBag.Title = "Cervejas";
-            ViewBag.Message = "Adicionar Cervejas";
+            ViewBag.Cervejas = cervejas.BuscarTodas();
 
             return View();
         }
 
-        
-        [HttpPost]
-
-        public void Salvar()
+        public ActionResult Detalhes(int id)
         {
+            var cervejas = new CervejasRepositorio();
 
+            ViewBag.Cerveja = cervejas.BuscarPorId(id);
 
-            var cerveja = new Cerveja();
-            cerveja.Id =Convert.ToInt32("0" + Request["id"]);
-            cerveja.Nome = Request["nome"];
-            cerveja.Tipo = Request["tipo"];
-            cerveja.Ano = Convert.ToInt16(Request["ano"]);
-            cerveja.Fabricacao = Convert.ToInt16(Request["fabricacao"]);
-            cerveja.Cor = Request["cor"];
-            cerveja.Embalagem = Convert.ToByte(Request["embalegem"]);
-            cerveja.Alcoolica = true;
-          
-            cerveja.Valor = Convert.ToDecimal(Request["valor"]);
-            cerveja.Ativo = true;
-
-            cerveja.Salvar();
-
-            Response.Redirect("/Home/Cervejas");
-
+            return View(cervejas.BuscarPorId(id));
         }
-
-
-        public ActionResult Alterar(int id)
-        {
-
-            ViewBag.Title = "Cerveja";
-            ViewBag.Message = "Alterar Cerveja"  + id;
-
-            var cervejas = new Cerveja();
-            cervejas.GetCervejas(id);
-            ViewBag.Cervejas = cervejas;
-
-            return View();
-        }
-
-        public ActionResult Excluir(int id)
-        {
-
-            ViewBag.Title = "Cerveja";
-            ViewBag.Message = "Excluir Cerveja" + id;
-
-            var cervejas = new Cerveja();
-            cervejas.GetCervejas(id);
-             ViewBag.Cervejas = cervejas;
-
-            return View();
-        }
-
-        [HttpPost]
-        public void Excluir()
-        {
-            var cervejas = new Cerveja();
-            cervejas.Id = Convert.ToInt32("0" + Request["id"]);
-            cervejas.Excluir();
-            Response.Redirect("/Home/Cervejas");
-
-
-        }
-
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,39 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using ProvaTLP.Models;
 
-
-
-
-namespace ProvaTLP.Models
+namespace ProvaTLP.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult Cervejas()
-        {
-            ViewBag.Title = "Cervejas artesanais";
-            ViewBag.Message = "Menu de Cervejas";
-
-
-            var lista = Cerveja.GetCerveja();
-            ViewBag.Lista = lista;
-
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-
-            ViewBag.Title = "Informações para Contato";
-            ViewBag.Message = "Cervejaria artesanal Vikings ® ";
-
             return View();
         }
     }
